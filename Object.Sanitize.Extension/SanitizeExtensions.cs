9e11e3664306eb8220fa21e203cb97fbc50474f1
Arthur Fanti <fanti1997@gmail.com>
2020-08-01 02:00:34 -0300

    Added Exception to string

--- Object.Sanitize.Extension/SanitizeExtensions.cs
+++ Object.Sanitize.Extension/SanitizeExtensions.cs
@@ -7,8 +7,12 @@ namespace Object.Sanitize.Extension
 {
     public static class SanitizeExtensions
     {
-        public static void Sanitize(this object sanitizable, params string[] notSanitizable)
+        public static void Sanitize<T>(this T sanitizable, params string[] notSanitizable) where T : class
         {
+            if (typeof(T) == typeof(string))
+            {
+                throw new NotImplementedException("Can't apply extension method for type string, use the HtmlSanitizer class to do it!");
+            }
             HtmlSanitizer sanitizer = new HtmlSanitizer();
             var sanitizableProps = Assembly
                .GetCallingAssembly()
